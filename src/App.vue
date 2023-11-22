e841a64e6167bae94752afe6396b9005d61522c4
MarcoGranata9 <granatam9900@gmail.com>
2023-11-22 16:00:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+    data() {
+        return {};
+    },
+    components: { AppTitle, AppTitle }
+}
+</script>
+
+<template>
+<h1>Vite</h1>
+<AppTitle/>
+</template>
+
+<style>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            text:"La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+<h2>{{ text }}</h2>
+</template>
+
+<style>
+
+</style>
